406aa82889b5dbb18cd7a65ebe3c6d3a3f894f91
psmarcin <ps@psmarcin.me>
2021-04-07 20:48:22 +0200

    feat: update dependencies (otel v0.18.0, fiber v2.7.1)

--- example/trace/main.go
+++ example/trace/main.go
@@ -10,11 +10,10 @@ func main() {
 	// configuration
 	// collector setup is out of the scope of this package so we don't focuse on it, just go to lightstep.com
 	// and get credential
-	otel := launcher.ConfigureOpentelemetry(
+	launcher.ConfigureOpentelemetry(
 		launcher.WithServiceName("example-basic"),
-		launcher.WithAccessToken("MISSING_TOKEN!!!"),
+		launcher.WithAccessToken("qpEupcxvxB4yfLqC0RLG1kf+2pa5afIeVhIR6mnE3pgSEV+K3V50/FyMGOHv3BhaL4c+haJg63Kr9Z4fa7adqczGCBFXlUHFcmybSMUu"),
 	)
-	defer otel.Shutdown()
 
 	// app setup
 	app := fiber.New()
